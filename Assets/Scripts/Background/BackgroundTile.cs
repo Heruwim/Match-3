7a24d2a1c34cd2e59bae0724138cad22d8e13dfa
Heruwim <heruwim25@gmail.com>
2023-12-28 12:08:22 +0200

    Create bacground and dots

--- Assets/Scripts/Background/BackgroundTile.cs
+++ Assets/Scripts/Background/BackgroundTile.cs
@@ -1,18 +1,19 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 
 public class BackgroundTile : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    [SerializeField] private GameObject[] _dots;
+
+    private void Start()
     {
-        
+        Initialize();
     }
 
-    // Update is called once per frame
-    void Update()
+    private void Initialize()
     {
-        
+        int dotToUse = Random.Range(0, _dots.Length);
+        GameObject dot = Instantiate(_dots[dotToUse], transform.position, Quaternion.identity);
+        dot.transform.parent = this.transform;
+        dot.name = this.gameObject.name;
     }
 }
--- Assets/Scripts/Board/Board.cs
+++ Assets/Scripts/Board/Board.cs
@@ -1,6 +1,30 @@
-using UnityEngine;
+    using UnityEngine;
 
 public class Board : MonoBehaviour
 {
-    
+    [SerializeField] private int _width;
+    [SerializeField] private int _height;
+    [SerializeField] private GameObject _tilePrefab;
+
+    private BackgroundTile[,] _allTiles;
+
+    private void Start()
+    {
+        _allTiles = new BackgroundTile[_width, _height];
+        SetUp();
+    }
+
+    private void SetUp()
+    {
+        for (int i = 0; i < _width; i++)
+        {
+            for (int j = 0; j < _height; j++)
+            {
+                Vector2 tempPosition = new Vector2(i, j);
+                GameObject backgroundTile = Instantiate(_tilePrefab, tempPosition, Quaternion.identity) as GameObject;
+                backgroundTile.transform.parent = this.transform;
+                backgroundTile.name = "(" + i + ", " + j + ")";
+            }
+        }
+    }
 }
